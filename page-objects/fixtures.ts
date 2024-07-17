b52093925a0ddd6207ce77a6d62c60e3ecdf0242
Yurii Tsymbala <tsymbala.yuriy@gmail.com>
2024-07-17 23:19:41 +0300

    Added API tests for Store

--- page-objects/fixtures.ts
+++ page-objects/fixtures.ts
@@ -1,9 +1,11 @@
 
 import { test as base } from '@playwright/test';
 import { PetApi } from '../page-objects/petApi';
+import { StoreApi } from './storeApi';
 
 type PetFixture = {
   petApi: PetApi;
+  storeApi: StoreApi;
 };
 
 export const test = base.extend<PetFixture>({
@@ -12,6 +14,13 @@ export const test = base.extend<PetFixture>({
     const api = new PetApi(request, baseURL);
     await use(api);
   },
+
+  storeApi: async ({ request }, use) => {
+    const baseURL = 'https://petstore.swagger.io/v2';
+    const api = new StoreApi(request, baseURL);
+    await use(api);
+  },
+
 });
 
 export { expect } from '@playwright/test';
--- testData.ts
+++ testData.ts
@@ -13,3 +13,18 @@ export const statuses = ['available', 'pending', 'sold'];
 
 export const updatedName = 'updatedNameTest';
 export const updatedStatus = 'pending';
+
+export const inventoryResponseValues = [
+  'sold', 'string', 'NewPets', 'pending', 'available', 'peric'
+];
+
+export const orderId = 1;
+
+export const newOrder = {
+  id: orderId,
+  petId: petId,
+  quantity: 10,
+  shipDate: "2024-07-17T18:51:02.555+0000",
+  status: "placed",
+  complete: true
+};
